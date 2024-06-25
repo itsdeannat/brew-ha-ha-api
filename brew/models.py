4fd87c6c48862bb457c3f84bb8f0ca293cc27f11
Deanna Thompson <deanna.thompson45@outlook.com>
2024-06-25 16:17:39 -0400

    feat: Add models for snacks and drinks

--- brew/models.py
+++ brew/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Drink(models.Model):
+    type = models.CharField(max_length=200)
+    temperature = models.CharField(max_length=500)
+    caffeine_amount = models.CharField(max_length=200)
+    price = models.CharField(max_length=500)    
+    
+class Snack(models.Model):
+    type = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200, null=True)
+    price = models.CharField(max_length=500)  
